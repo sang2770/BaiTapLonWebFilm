81d0f4a34d85872de8d161bf3e113f40491e89f5
hung <hung71718@gmail.com>
2022-04-12 16:41:24 +0700

    phantrang lich

--- BaiTapLonWebFilm/Areas/Admin/Controllers/LichChieuController.cs
+++ BaiTapLonWebFilm/Areas/Admin/Controllers/LichChieuController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using BaiTapLonWebFilm.Models;
+using PagedList;
 
 namespace BaiTapLonWebFilm.Areas.Admin.Controllers
 {
@@ -15,10 +16,48 @@ namespace BaiTapLonWebFilm.Areas.Admin.Controllers
         private DBFilmEntities1 db = new DBFilmEntities1();
 
         // GET: Admin/LichChieu
-        public ActionResult Index()
+        public ActionResult Index(int? size, int? page)
         {
-            var tB_LICHCHIEU = db.TB_LICHCHIEU.Include(t => t.TB_PHIM).Include(t => t.TB_PHONG);
-            return View(tB_LICHCHIEU.ToList());
+            // 1. Tạo list pageSize để người dùng có thể chọn xem để phân trang
+            // Bạn có thể thêm bớt tùy ý 
+            List<SelectListItem> items = new List<SelectListItem>();
+            items.Add(new SelectListItem { Text = "5", Value = "5" });
+            items.Add(new SelectListItem { Text = "10", Value = "10" });
+            items.Add(new SelectListItem { Text = "20", Value = "20" });
+            items.Add(new SelectListItem { Text = "25", Value = "25" });
+            items.Add(new SelectListItem { Text = "50", Value = "50" });
+            items.Add(new SelectListItem { Text = "100", Value = "100" });
+            items.Add(new SelectListItem { Text = "200", Value = "200" });
+
+            // 1.1. Giữ trạng thái kích thước trang được chọn trên DropDownList
+            foreach (var item in items)
+            {
+                if (item.Value == size.ToString()) item.Selected = true;
+            }
+
+            // 1.2. Tạo các biến ViewBag
+            ViewBag.size = items; // ViewBag DropDownList
+            ViewBag.currentSize = size; // tạo biến kích thước trang hiện tại
+
+            // 2. Nếu page = null thì đặt lại là 1.
+            page = page ?? 1; //if (page == null) page = 1;
+
+            // 3. Tạo truy vấn, lưu ý phải sắp xếp theo trường nào đó, ví dụ OrderBy
+            // theo LinkID mới có thể phân trang.
+            var links = (from l in db.TB_LICHCHIEU
+                         select l).OrderBy(x => x.MALICHCHIEU);
+
+            // 4. Tạo kích thước trang (pageSize), mặc định là 5.
+            int pageSize = (size ?? 5);
+
+            // 4.1 Toán tử ?? trong C# mô tả nếu page khác null thì lấy giá trị page, còn
+            // nếu page = null thì lấy giá trị 1 cho biến pageNumber.
+            int pageNumber = (page ?? 1);
+
+            // 5. Trả về các Link được phân trang theo kích thước và số trang.
+            return View(links.ToPagedList(pageNumber, pageSize));
+            // return View(db.TB_PHIM.ToList());
+           
         }
 
         // GET: Admin/LichChieu/Details/5
@@ -76,7 +115,7 @@ namespace BaiTapLonWebFilm.Areas.Admin.Controllers
                 return HttpNotFound();
             }
             ViewBag.MAPHIM = new SelectList(db.TB_PHIM, "MAPHIM", "TENPHIM");
-            ViewBag.MAPHONG = new SelectList(db.TB_PHONG, "MAPHONG", "LOAIPHONG");
+            ViewBag.MAPHONG = new SelectList(db.TB_PHONG, "MAPHONG", "MAPHONG");
             return View(tB_LICHCHIEU);
         }
 
